7d965be3205607421967a90fb7ef333e694c3754
Jaro Habiger <jarohabiger@googlemail.com>
2022-04-26 00:15:31 +0200

    add bold variant

--- fontforge_import.py
+++ fontforge_import.py
@@ -10,34 +10,36 @@ from math import ceil
 top = 33
 middle = 50
 bottom = 81
-extent = 4
+extent = 6
 
-font = fontforge.font()
+# font = fontforge.font()
+font = fontforge.open('happy_virus.sfd')
 font.em = top + middle + bottom
 font.ascent = top + middle
 font.descent = bottom
 
-all_svg = etree.parse('happy_virus.svg')
+for variant in ('happy_virus', 'happy_virus_bold'):
+    all_svg = etree.parse(f'{variant}.svg')
+    for c in ascii_lowercase:
+        element = all_svg.xpath(f'//*[@id="{c}"]')[0]
+        d = parse_path(element.get("d"))
+        xmin, xmax, ymin, ymax = d.bbox()
+        d = d.translated(complex(-xmin + extent))
+        
+        glyph = font.createChar(ord(c))
+        glyph.clear()
+        glyph.width = int(ceil(xmax - xmin + extent * 3))
 
-for c in ascii_lowercase:
-    element = all_svg.xpath(f'//*[@id="{c}"]')[0]
-    d = parse_path(element.get("d"))
-    xmin, xmax, ymin, ymax = d.bbox()
-    d = d.translated(complex(-xmin + extent))
-    
-    glyph = font.createChar(ord(c))
-    glyph.width = int(ceil(xmax - xmin + extent * 2))
+        with tempfile.NamedTemporaryFile(suffix=".svg") as tmp:
+            element.set("d", d.d())
+            el_string = etree.tostring(element)
+            tmp.write(f"""
+            <svg>
+                {el_string}
+            </svg>
+            """.encode())
+            tmp.flush()
+            glyph.importOutlines(tmp.name)
+            glyph.transform(psMat.translate(0, -top))
 
-    with tempfile.NamedTemporaryFile(suffix=".svg") as tmp:
-        element.set("d", d.d())
-        el_string = etree.tostring(element)
-        tmp.write(f"""
-        <svg>
-            {el_string}
-        </svg>
-        """.encode())
-        tmp.flush()
-        glyph.importOutlines(tmp.name)
-        glyph.transform(psMat.translate(0, -top))
-
-font.save('happy_virus.sfd')
+    font.save(f'{variant}.sfd')
